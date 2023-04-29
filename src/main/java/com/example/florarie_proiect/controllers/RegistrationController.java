c7a0bfcfb58c34ab6ce0fce797b121a546a006bc
Alex <alexpetrescu183@gmail.com>
2023-04-29 12:50:20 +0300

    FP-28 Added the login and register screens

---
+++ src/main/java/com/example/florarie_proiect/controllers/RegistrationController.java
@@ -0,0 +1,36 @@
+package com.example.florarie_proiect.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import com.example.florarie_proiect.exceptions.UsernameAlreadyExists;
+import com.example.florarie_proiect.services.UserService;
+
+public class RegistrationController {
+
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Client", "Admin");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), (String) role.getValue());
+            registrationMessage.setText("Account created successfully!");
+        } catch (UsernameAlreadyExists e) {
+            registrationMessage.setText(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/example/florarie_proiect/exceptions/CouldNotWriteUsers.java
@@ -0,0 +1,4 @@
+package com.example.florarie_proiect.exceptions;
+
+public class CouldNotWriteUsers extends RuntimeException{
+}
