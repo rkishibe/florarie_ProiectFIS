b49a8f43bcad58e66a9b69057bf0e8e6fdc14617
paula <paula.pistrila@student.upt.ro>
2023-05-11 01:06:03 +0300

    FP-4 Designed UI for card details

--- src/main/java/florarie/controllers/CardCreditControler.java
+++ src/main/java/florarie/controllers/CardCreditControler.java
@@ -1,37 +1,101 @@
-package florarie.controllers;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-import florarie.model.CardCredit;
+import java.util.Calendar;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+public class CardController {
 
-public class CardCreditControler {
-        public static boolean verificareCard(CardCredit card) {
-            if (card.getNumarCard().length() != 16) {
-                return false;
+    @FXML
+    private TextField numarCardField;
+
+    @FXML
+    private TextField numeDetinatorField;
+
+    @FXML
+    private TextField dataExpirareField;
+
+    @FXML
+    private TextField codSecuritateField;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    void initialize() {
+        sendButton.setOnAction(event -> {
+            String numarCard = numarCardField.getText();
+            String numeDetinator = numeDetinatorField.getText();
+            String dataExpirare = dataExpirareField.getText();
+
+            // verificam daca datele sunt valide
+            boolean isDataValid = isCardDataValid(numarCard, numeDetinator, dataExpirare, codSecuritateField.getText());
+
+            if (isDataValid) {
+                // daca datele sunt valide, ne intoarcem la pagina de home
+                // aici puteti inlocui "Home.fxml" cu numele fisierului FXML al paginii home din aplicatia voastra
+               // SceneChanger.changeScene(sendButton, "Home.fxml");
+                Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+                Scene scene = new Scene(root, 800, 600);
+                Stage stage = (Stage) sendButton.getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+
+
+            } else {
+                // daca datele sunt invalide, afisam un mesaj de eroare
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Invalid card data");
+                alert.setHeaderText(null);
+                alert.setContentText("Datele cardului sunt invalide!");
+                alert.showAndWait();
             }
-
-            if (card.getNumeDetinator().isEmpty()) {
-                return false;
-            }
-
-
-            SimpleDateFormat formatter = new SimpleDateFormat("MM/yy");
-            Date currentDate = new Date();
-            try {
-                Date dataExpirare = formatter.parse(card.getDataExpirare());
-                if (currentDate.after(dataExpirare)) {
-                    return false;
-                }
-            } catch (ParseException e) {
-                return false;
-            }
-
-            if (card.getCodSecuritate() < 100 || card.getCodSecuritate() > 999) {
-                return false;
-            }
-
-            return true;
-        }
+        });
     }
+
+    private boolean isCardDataValid(String numarCard, String numeDetinator, String dataExpirare, String codSecuritate) {
+        // verificam daca toate campurile sunt completate
+        if (numarCard.isEmpty() || numeDetinator.isEmpty() || dataExpirare.isEmpty() || codSecuritate.isEmpty()) {
+            return false;
+        }
+
+        // verificam daca numarul de card are 16 cifre
+        if (!numarCard.matches("\\d{16}")) {
+            return false;
+        }
+
+        // verificam daca codul de securitate are 3 cifre
+        if (!codSecuritate.matches("\\d{3}")) {
+            return false;
+        }
+
+        // verificam daca data de expirare este valida
+        try {
+            // parsam data in formatul "MM/YY"
+            int expMonth = Integer.parseInt(dataExpirare.substring(0, 2));
+            int expYear = Integer.parseInt(dataExpirare.substring(3));
+
+            // obtinem data curenta
+            Calendar calendar = Calendar.getInstance();
+            int currentYear = calendar.get(Calendar.YEAR) % 100; // luam doar ultimele 2 cifre ale anului
+            int currentMonth = calendar.get(Calendar.MONTH) + 1; // lunile incep de la 0 in Calendar, asa ca trebuie sa adaugam 1
+
+            // verificam daca data de expirare este in viitor
+            if (expYear < currentYear || (expYear == currentYear && expMonth < currentMonth)) {
+                return false;
+            }
+        } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
+            // daca data nu poate fi parsata, inseamna ca nu e valida
+            return false;
+        }
+
+        // daca toate verificarile au trecut, inseamna ca datele sunt valide
+        return true;
+    }
+
+}
