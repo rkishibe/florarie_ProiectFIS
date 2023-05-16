eb7ada787521415396af995e22ceb76d6ce9889f
paula <paula.pistrila@student.upt.ro>
2023-05-16 22:21:14 +0300

    Am facut pagina de AdminHome si ClientHome

--- src/main/java/com/example/florarie_proiect/controllers/LoginController.java
+++ src/main/java/com/example/florarie_proiect/controllers/LoginController.java
@@ -32,7 +32,7 @@ public class LoginController {
         role.getItems().addAll("Client", "Admin");
     }
     @FXML
-    public void handleLoginButtonAction() {
+    public void handleLoginButtonAction(ActionEvent event) {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
@@ -41,8 +41,18 @@ public class LoginController {
             if(UserService.checkPassword(usernameField.getText(), passwordField.getText(), usernameField.getText())){
                 if(role.getValue()=="Client"){
                     loginMessage.setText("Logat ca si client.");
+                    Parent homeRoot = FXMLLoader.load(getClass().getResource("/com/example/florarie_proiect/ClientHome.fxml"));
+                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    Scene scene = new Scene(homeRoot);
+                    stage.setScene(scene);
+                    stage.show();
                 } else if(role.getValue()=="Admin"){
                     loginMessage.setText("Logat ca si admin.");
+                    Parent homeRoot = FXMLLoader.load(getClass().getResource("/com/example/florarie_proiect/AdminHome.fxml"));
+                    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    Scene scene = new Scene(homeRoot);
+                    stage.setScene(scene);
+                    stage.show();
                 }
             }
         }catch (Exception e){
