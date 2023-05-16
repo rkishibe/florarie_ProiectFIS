e70c8156346be63ab90f7d8868ca8e67aa698b1d
paula <paula.pistrila@student.upt.ro>
2023-05-16 18:04:20 +0300

    fixed cancel buttons

--- src/main/java/com/example/florarie_proiect/controllers/LoginController.java
+++ src/main/java/com/example/florarie_proiect/controllers/LoginController.java
@@ -4,7 +4,9 @@ import com.example.florarie_proiect.services.UserService;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.scene.control.ChoiceBox;
@@ -12,6 +14,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -58,7 +62,7 @@ public class LoginController {
         loginMessage.setText("Parola sau username gresite!");
     }
 
-
+/*
     public void handleCancelButtonAction(ActionEvent event) throws Exception {
         // Get the current scene
         Scene currentScene = ((Node) event.getSource()).getScene();
@@ -77,5 +81,14 @@ public class LoginController {
             stage.show();
         }
 
+    }*/
+
+    public void switchToSceneHome(ActionEvent event) throws IOException
+    {
+        Parent homeRoot = FXMLLoader.load(getClass().getResource("/com/example/florarie_proiect/home.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(homeRoot);
+        stage.setScene(scene);
+        stage.show();
     }
 }
